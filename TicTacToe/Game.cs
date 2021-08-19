0068ba0b78749273c8fb1f209b57009a374bfc45
Nicolas Aguilera García <niagar25@gmail.com>
2021-08-18 21:25:10 -0500

    Start of development of Tic Tac Toe game

---
+++ TicTacToe/Game.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    class Game
+    {
+        private char[,] board = 
+        {
+            { '0', '1', '2' },
+            { '3', '4', '5' },
+            { '6', '7', '8' }
+        };
+
+        private int player = 1;
+
+        public Game() { }
+
+        public void Start()
+        {
+                DrawBoard();
+
+                var cell = Console.ReadKey();
+                Console.WriteLine(cell);
+            /*
+            do
+            {
+
+                
+            } while (true); */
+        }
+
+        public void ChangeCellValue(int x, int y, char value)
+        {
+            board[x, y] = value;
+        } 
+
+        public void DrawBoard()
+        {
+            char a, b, c;
+            
+            var whiteLine = "|  {0}  |  {1}  |  {2}  |";
+            var line = "|-----|-----|-----|";
+
+            for (int i = 0; i < this.board.GetLength(0); i++)
+            {
+                a = this.board[i, 0];
+                b = this.board[i, 1];
+                c = this.board[i, 2];
+                Console.WriteLine(line);
+                Console.WriteLine(whiteLine, a, b, c);
+            }
+
+            Console.WriteLine(line);
+        }
+    }
+}
